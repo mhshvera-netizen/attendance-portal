dd59be0950ebe47649146752a309c270bab47756
mhshvera-netizen <mhshvera-netizen@users.noreply.github.com>
2026-08-18 20:07:05 +0000

    bisect2: scraper import test

--- api/index.py
+++ api/index.py
@@ -3,13 +3,15 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 from flask import Flask, Response
 try:
     import app_page
-    MSG = 'app_page OK len=%d' % len(app_page.app_page_html())
+    import scraper
+    MSG = 'all imports OK'
 except Exception as e:
-    MSG = 'IMPORT FAIL: %r' % e
+    import traceback
+    MSG = 'IMPORT FAIL: ' + traceback.format_exc()
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/api/index', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
-    return Response(MSG, mimetype='text/plain')
+    return Response('<pre>%s</pre>' % MSG, mimetype='text/html')
